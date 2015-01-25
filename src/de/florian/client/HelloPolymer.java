7bb038aaecb023914a45c40efe8b7783bb347ca2
Florian Friemel <florianfriemel@gmail.com>
2015-01-25 18:49:28 +0100

    Hello Polymer!

---
+++ src/de/florian/client/HelloPolymer.java
@@ -0,0 +1,18 @@
+package de.florian.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.Composite;
+
+
+public class HelloPolymer extends Composite
+{
+  interface MyUiBinder extends UiBinder<Widget, HelloPolymer> {}
+
+  private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
+
+  public HelloPolymer() {
+    initWidget(uiBinder.createAndBindUi(this));
+  }
+}
